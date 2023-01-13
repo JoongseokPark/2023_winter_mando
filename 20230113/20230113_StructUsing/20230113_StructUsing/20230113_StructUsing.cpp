71ab55f2a053ca6387558324123c35d6aa9cea44
JoongseokPark <112610049+JoongseokPark@users.noreply.github.com>
2023-01-13 12:51:47 +0900

    Add files via upload

---
+++ 20230113/20230113_StructUsing/20230113_StructUsing/20230113_StructUsing.cpp
@@ -0,0 +1,61 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+
+struct student {
+	int num;
+	char name[20];
+	int kor;
+	int eng;
+	int math;
+	int sum;
+	float avg;
+};
+void score_prt(struct student* p);
+void score_gene(struct student* p);
+float round(float a);
+char randName[20][20] = { "김성윤","최강민","김경수","박현수","박길태","백두산","신성하","허준","이태식","구대문","김수광","이광수"};
+
+int main()
+{
+	srand((unsigned)time(NULL));
+	struct student s_list[10];
+
+	score_gene(s_list);
+	score_prt(s_list);
+}
+
+float round(float a) {
+	float left = a/10 - int(a/10);
+	if (left > 0.5) a += float((10 - int(left*10))) * 10;
+	a -= left * 10;
+	a = float(a)/1000;
+	return a;
+}
+
+void score_gene(struct student *p) {
+	for (int i = 0;i < 10;i++) {
+		strcpy_s((p + i)->name, randName[rand() % 12]);
+		(p + i)->kor = (rand() % 101);
+		(p + i)->eng = (rand() % 101);
+		(p + i)->math = (rand() % 101);
+		(p + i)->sum = (p + i)->kor + (p + i)->eng + (p + i)->math;
+		(p + i)->avg = float(((p + i)->kor + (p + i)->eng + (p + i)->math)) / 3;
+		(p + i)->avg = round((p + i)->avg*1000); //평균을 소수점 둘째자리에서 반올림하는 기능
+	}
+}
+
+void score_prt(struct student* p) {
+	printf("\n번호\t이름\t국어\t영어\t수학\t총점\t평균\n");
+	for (int i = 0;i < 10;i++) {
+		printf("%d\t", i+1);
+		printf("%s\t", (p + i)->name);
+		printf("%d\t", (p + i)->kor);
+		printf("%d\t", (p + i)->eng);
+		printf("%d\t", (p + i)->math);
+		printf("%d\t", (p + i)->sum);
+		printf("%f\n", (p + i)->avg);
+	}
+}
+
